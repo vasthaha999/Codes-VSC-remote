f5f98b89dee1106a51924080be30c5f0c35d79bb
vasthaha999 <2427889065@qq.com>
2023-10-02 11:53:53 +0800

    VSC

---
+++ C_learning/badcount/badcount.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n = 4;
+    int m = 5;
+    float f = 7.0f;
+    float g = 8.0f;
+
+    printf("%d\n", n, m);
+    printf("%d %d %d\n", n);
+    printf("%d %d", f, g);
+
+    return 0;
+}
---
+++ C_learning/typesize/typesize.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    printf("Type int has a size of %zd bytes.\n", sizeof(int));
+    printf("Type char has a size of %zd bytes.\n", sizeof(char));
+    printf("Type long has a size of %zd bytes.\n", sizeof(long));
+    printf("Type long long has a size of %zd bytes.\n", sizeof(long long));
+    printf("Type double has a size of %zd bytes.\n", sizeof(double));
+    printf("Type long double has a size of %zd bytes.\n", sizeof(long double));
+
+    return 0;
+}
+// sizeof 以字节为单位给出指定类型的大小， %zd 转换说明匹配sizeof的返回类型
